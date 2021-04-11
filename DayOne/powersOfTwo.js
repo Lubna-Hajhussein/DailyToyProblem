6f0df4bb0561adfcbb7ba40aa10afab3da741512
Lubna-Hajhussein <lubnahaj45@gmail.com>
2021-04-11 14:51:22 +0300

    DayOne-powersOfTwo

---
+++ DayOne/powersOfTwo.js
@@ -0,0 +1,4 @@
+//Complete the function that takes a non-negative integer n as input, and returns a list of all
+// the powers of 2 with the exponent ranging from 0 to n (inclusive).
+
+const powersOfTwo = (n) => Array.from({length: n+1}, (v, k) => 2**k); 
